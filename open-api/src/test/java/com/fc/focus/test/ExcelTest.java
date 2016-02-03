1d762f10c965179d0664bd20711290994e7736f4
黄旺 <shhuangwang@corp.netease.com>
2016-02-03 11:43:33 +0800

    添加测试类型判断

--- open-api/src/test/java/com/fc/focus/test/ExcelTest.java
+++ open-api/src/test/java/com/fc/focus/test/ExcelTest.java
@@ -1,17 +1,22 @@
 package com.fc.focus.test;
 
 import com.fc.focus.api.common.ExcelUtil;
+import com.fc.focus.api.common.TestCaseExcel;
 import com.fc.focus.api.http.HttpUtils;
+import groovy.util.ResourceException;
+import groovy.util.ScriptException;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
-import java.util.Map;
+import java.io.IOException;
 
 /**
- * Created by hwa on 2016/1/29.
+ * Note : Excel测试接口
+ * Author : hwa
+ * Date : 2016/2/3
  */
 public class ExcelTest {
 
@@ -21,20 +26,49 @@ public class ExcelTest {
         return initTestCaseExcel();
     }
 
-
     @Test(dataProvider = "dataProvider")
-    public void doTest(String url, String params, Map<String, String> header, String resBody, int httpCode) throws Exception {
+    public void doTest(TestCaseExcel testCase) throws IOException, ResourceException, ScriptException {
+        String assertType = testCase.getAssertType();
+        if (assertType.equals("HC")) {
+            if (testCase.getMethod().equals("GET")) {
+                HttpUtils.HttpResult result = HttpUtils.get(testCase);
+                int statusCode = result.getStatusCode();
+                Assert.assertEquals(statusCode + "", testCase.getExpected());
+            } else {
+                HttpUtils.HttpResult result = HttpUtils.post(testCase);
+                int statusCode = result.getStatusCode();
+                Assert.assertEquals(statusCode + "", testCase.getExpected());
+            }
+        } else if (assertType.equals("RB")) {
+            if (testCase.getMethod().equals("GET")) {
+                HttpUtils.HttpResult result = HttpUtils.get(testCase);
+                String responseBody = new String(result.getResponseBody());
+                Assert.assertEquals(responseBody, testCase.getExpected());
+            } else {
+                HttpUtils.HttpResult result = HttpUtils.post(testCase);
+                String responseBody = new String(result.getResponseBody());
+                Assert.assertEquals(responseBody, testCase.getExpected());
+            }
+        } else if (assertType.equals("GV")) {
+            if (testCase.getMethod().equals("GET")) {
+                HttpUtils.HttpResult result = HttpUtils.get(testCase);
+                String responseBody = new String(result.getResponseBody());
 
-        HttpUtils.HttpResult result = HttpUtils.post(url, params, header);
-        String responseBody = new String(result.getResponseBody());
-        int statusCode = result.getStatusCode();
+                Object groovyResult = ExcelUtil.getGroovyResult(responseBody, testCase.getGroovyScript());
+                Assert.assertEquals(groovyResult.toString(), testCase.getExpected());
+            } else {
+                HttpUtils.HttpResult result = HttpUtils.post(testCase);
+                String responseBody = new String(result.getResponseBody());
 
-        Assert.assertEquals(statusCode, httpCode);
-
+                Object groovyResult = ExcelUtil.getGroovyResult(responseBody, testCase.getGroovyScript());
+                Assert.assertEquals(groovyResult.toString(), testCase.getExpected());
+            }
+        } else {
+            Assert.fail("断言类型错误，请检查Excel");
+        }
     }
 
-
-    private Object[][] initTestCaseExcel() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    private Object[][] initTestCaseExcel() {
 
         try {
             XSSFSheet sheet = ExcelUtil.getSheet();
