1fe09af9b9b9355a6c4681af34d883eab678230f
黄旺 <shhuangwang@corp.netease.com>
2016-02-03 11:40:43 +0800

    新增Groovy脚本执行

--- open-api/src/test/java/com/fc/focus/api/common/ExcelUtil.java
+++ open-api/src/test/java/com/fc/focus/api/common/ExcelUtil.java
@@ -1,15 +1,23 @@
 package com.fc.focus.api.common;
 
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import groovy.util.GroovyScriptEngine;
+import groovy.util.ResourceException;
+import groovy.util.ScriptException;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
+import java.util.UUID;
 
 /**
- * Created by hwa on 2016/1/27.
+ * Note : 解析Excel工具类
+ * Author : hwa
+ * Date : 2016/1/27
  */
 public class ExcelUtil {
 
@@ -18,7 +26,6 @@ public class ExcelUtil {
     public static XSSFSheet getSheet() throws FileNotFoundException {
 
         String filePath = PropertiesUtil.getProperties().getProperty("excelUrl");
-
         XSSFWorkbook wb = null;
         try {
             wb = new XSSFWorkbook(filePath);
@@ -33,33 +40,108 @@ public class ExcelUtil {
     //解析每一行数据
     public static Object[][] getRowData(Sheet sheet) {
 
-        int rowNums = sheet.getLastRowNum();
-        Object[][] dataProvider = new Object[rowNums][5];
+        int rowNums = getRealRowNum(sheet) - 1;
+        Object[][] dataProvider = new Object[rowNums][1];
         for (int i = 1; i < rowNums + 1; i++) {
-            //解析request
-            String URL = sheet.getRow(i).getCell(0).toString();
-            String paramJson = sheet.getRow(i).getCell(1).toString();
-            String str = sheet.getRow(i).getCell(2).toString();
+
+            TestCaseExcel testCase = new TestCaseExcel();
+
+            String URL = sheet.getRow(i).getCell(1).toString();
+            String paramJson = sheet.getRow(i).getCell(2).toString();
+
+            String str = sheet.getRow(i).getCell(3).toString();
             String[] headers = str.split("\\r\\n");
             HashMap<String, String> header = new HashMap<String, String>();
             for (String s : headers) {
                 header.put(s.substring(0, s.indexOf(":")), s.substring(str.indexOf(":") + 1, s.length()));
             }
 
-            //解析Response
-            String responseBody = sheet.getRow(i).getCell(3).toString();
-            int statusCode = (int) sheet.getRow(i).getCell(4).getNumericCellValue();
+            String method = sheet.getRow(i).getCell(4).toString();
+            String auth = sheet.getRow(i).getCell(5).toString();
+            String groovyScript = "";
+            if (sheet.getRow(i).getCell(6) != null) {
+                groovyScript = sheet.getRow(i).getCell(6).toString();
+            }
+            String assertType = sheet.getRow(i).getCell(7).toString();
+
+            String expected = "";
+            if (sheet.getRow(i).getCell(8).getCellType() == Cell.CELL_TYPE_NUMERIC) {
+                Object obj = (Object) Math.round(sheet.getRow(i).getCell(8).getNumericCellValue());
+                expected = obj.toString();
+            } else {
+                expected = sheet.getRow(i).getCell(8).toString();
+            }
+
+
+            testCase.setUrl(URL);
+            testCase.setParamJson(paramJson);
+            testCase.setHeader(header);
+            testCase.setMethod(method);
+            testCase.setAuth(auth);
+            testCase.setGroovyScript(groovyScript);
+            testCase.setAssertType(assertType);
+            testCase.setExpected(expected);
 
             //将数据放入[][]
-            dataProvider[i - 1][0] = URL;
-            dataProvider[i - 1][1] = paramJson;
-            dataProvider[i - 1][2] = header;
-            dataProvider[i - 1][3] = responseBody;
-            dataProvider[i - 1][4] = statusCode;
-
+            dataProvider[i - 1][0] = testCase;
         }
-
         return dataProvider;
+    }
+
+
+    //使用GroovyScriptEngine执行Groovy脚本
+    public static Object getGroovyResult1(String json, String fileName) throws IOException, ResourceException, ScriptException {
+
+        String dir = Class.class.getClass().getResource("/").getPath();
+        String[] roots = new String[]{dir};
+
+        GroovyScriptEngine engine = new GroovyScriptEngine(roots);
+
+        Binding binding = new Binding();
+        binding.setVariable("json", json);
+        Object result = engine.run(fileName, binding);
+        return result;
+    }
+
+    //使用GroovyShell.eval()执行Groovy脚本
+    public static Object getGroovyResult(String json, String script) throws IOException, ResourceException, ScriptException {
+
+        Binding bind = new Binding();
+        bind.setVariable("json", json);
+        GroovyShell shell = new GroovyShell(bind);
+        Object result = shell.evaluate(script);
+
+        return result;
+    }
+
+    public static String generateGroovyScript(String script) throws IOException {
+        String dir = Class.class.getClass().getResource("/").getPath();
+        String filename = "a" + UUID.randomUUID().toString().replace("-", "") + ".groovy";
+        FileWriter fileWriter = new FileWriter(dir + filename);
+        BufferedWriter writer = new BufferedWriter(fileWriter);
+        writer.write(script);
+        writer.close();
+        return filename;
+    }
+
+    public static void deleteGroovyFile(String filePath) {
+        String dir = Class.class.getClass().getResource("/").getPath();
+        File file = new File(dir + filePath);
+        if (file.isFile()) {
+            file.delete();
+        }
+    }
+
+    public static int getRealRowNum(Sheet sheet) {
+
+        int lastRowNum = sheet.getLastRowNum();
+        int count = 0;
+        for (int i = 0; i <= lastRowNum; i++) {
+            if (sheet.getRow(i).getCell(1).toString() != "") {
+                count++;
+            }
+        }
+        return count;
     }
 
 
@@ -68,5 +150,6 @@ public class ExcelUtil {
         getRowData(getSheet());
 
     }
+
 
 }
